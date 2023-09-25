6ecc211bbbca7f6ee90d26c134324acb8aaea588
Rajesh kumar halder <82487522+Rajesh144142@users.noreply.github.com>
2023-09-25 20:53:52 +0530

    Update constant.js
    constant changed

--- frontend/src/components/utils/constant.js
+++ frontend/src/components/utils/constant.js
@@ -1,1 +1,3 @@
-export const baseUrl= "http://localhost:5000/"
+// export const baseUrl= "http://localhost:5000/"
+// https://anthektik-s.onrender.com
+export const baseUrl= "https://anthektik-s.onrender.com/"
