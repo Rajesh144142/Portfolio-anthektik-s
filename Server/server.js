47dbf39dddbdad8b50d99f82ae4749cb6be1f349
Rajesh144142 <rajeshkh704435@gmail.com>
2023-11-01 18:13:42 +0530

    Updated CodeBase

--- Server/server.js
+++ Server/server.js
@@ -12,26 +12,40 @@ require('dotenv').config();
 
 const app = express();
 
-// Middleware
+//--------------------Middleware---------------------------
 app.use(morgan('tiny'));
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json({ limit: '20mb' }));
 app.use(bodyParser.json({ limit: '30mb', extended: true }));
 app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
-///
-app.use(express.static(path.join(__dirname, './Client/build')))
-// Define your routes here
+
+
+
+
+
+//--------Define your routes here-----------
 app.post('/submit-otp', sendit.sendmessage);
 app.use('/auth', Authrouter);
 app.use('/Pro', Project);
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, './Client/build/index.html'))
-});
 
 
 
-// Database calling
+
+
+//------------deployment--------------------
+const __dirname1 = path.resolve();
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname1, "../Client/build")))
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname1, "../Client/build/index.html"))
+  });
+} else {
+  app.get("/", (req, res) => { res.send("API is Running Successfully...."); })
+}
+
+
+// --------------Database calling------------
 const port = process.env.PORT || 5000;
 connect()
   .then((response) => {
@@ -43,5 +57,4 @@ connect()
     console.log("Failed to connect to the database or start the server.");
   });
 
-// Routes
 
